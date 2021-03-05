beedf4401e13779d56a2947e7536d644bdaf5772
austin shields <ajshields196@gmail.com>
2021-03-05 04:09:58 -0500

    add validations to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, numericality: { greater_than: 1888 }
+  validates :plot, length: { in: 25..255 }
+  validates :director, presence: true
 end
