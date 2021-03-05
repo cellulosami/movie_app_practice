363c01b6d427a237d44580c196d8f9ff8660fb19
austin shields <ajshields196@gmail.com>
2021-03-05 03:38:59 -0500

    complete restful routes for actors

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,6 +1,36 @@
 class Api::ActorsController < ApplicationController
+  def index
+    @actors = Actor.all
+    render "index.json.jb"
+  end
+
   def show
-    @actor = Actor.find_by(id: params[:id])
+    @actor = Actor.find_by(id: params[:id].to_i)
     render 'show.json.jb'
+  end
+
+  def create
+    @actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for]
+    )
+    @actor.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @actor = Actor.find_by(id: params[:id])
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @actor = Actor.find_by(id: params[:id])
+    @actor.destroy
+    render json: {message: "The actor has been exsanguinated"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do
+    get "/actors" => "actors#index"
     get "/actors/:id" => "actors#show"
+    post "/actors" => "actors#create"
+    patch "actors/:id" => "actors#update"
+    delete "actors/:id" => "actors#destroy"
     
     get "/movies" => "movies#index"
     get "/movies/query" => "movies#show"
