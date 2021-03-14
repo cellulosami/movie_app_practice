956db6c02be8042963e1c9674c209530608e687f
austin shields <ajshields196@gmail.com>
2021-03-13 22:26:25 -0500

    add associations between movies and genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :genre_movies
+  has_many :genres, through: :genre_movies
   
   validates :title, presence: true
   validates :year, numericality: { greater_than: 1888 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_031211) do
+ActiveRecord::Schema.define(version: 2021_03_14_031523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2021_03_14_031211) do
     t.string "gender"
     t.integer "age"
     t.integer "movie_id"
+  end
+
+  create_table "genre_movies", force: :cascade do |t|
+    t.integer "genre_id"
+    t.integer "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "genres", force: :cascade do |t|
